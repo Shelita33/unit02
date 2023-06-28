287e2154c8bdc217f856da26c3c894929b5a7f6c
Shelita33 <135672845+Shelita33@users.noreply.github.com>
2023-06-28 00:02:47 -0500

    Create salary.kt

---
+++ salary.kt
@@ -0,0 +1,9 @@
+fun main()
+{
+		//String name = "Shelita Taylor";
+		//int age = 33;
+		//double annualPay = 99.99;
+	println("My name is Shelita Taylor."); 
+    println("My age is 33."); println("I hope to earn $100,000");
+    println (" per year.");
+	}
