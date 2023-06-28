aa3ba04eae3d86130f27f5765aa0603b7b5dbf77
Shelita33 <135672845+Shelita33@users.noreply.github.com>
2023-06-27 20:10:18 -0500

    marketing.kt

---
+++ marketing.kt
@@ -0,0 +1,12 @@
+ fun main() {
+	 println("Shelita Taylor")
+      println("marketing.kt")
+       println("6/27/23")
+        
+    val b: String? = "FriendsCount"
+    if (b != null && b.length > 0) {
+        print("Buy Cat Food At 20% Off")
+    } else {
+        print("Empty string")
+    }
+ }
