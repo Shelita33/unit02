06add7fe8ac6ee19d1be3dc30009ecdec0b1c259
Shelita33 <135672845+Shelita33@users.noreply.github.com>
2023-07-06 22:16:21 -0500

    variables.kt

---
+++ variables.kt
@@ -0,0 +1,13 @@
+fun main() {
+	var myAge = 33
+    val myDouble = 30; "averageAge"
+    val testNumber = 7
+	var evenOdd = "testNumber"
+	var answer = 1
+    
+    	println(myAge)
+        println("averageAge times 33 is" + (myDouble))
+        println(testNumber)
+        println(evenOdd)
+        println("The other number add 10 is" + (answer*10))
+}
